d949bd683a95591f7429dc04ecda1120ece2d4f9
Yordanni Ortiz <yordannimod@gmail.com>
2024-07-31 18:48:33 -0400

    Error message added to the reset password link.

--- src/pages/ResetPasswordForm.jsx
+++ src/pages/ResetPasswordForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import "../assets/styles/ResetPasswordForm.css";
 import Button from "react-bootstrap/Button";
@@ -8,6 +8,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye, faEyeSlash, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Loader from "../components/Loader";
 
 const ResetPasswordForm = () => {
   const { token } = useParams(); // Obtén el token de la URL
@@ -17,6 +18,32 @@ const ResetPasswordForm = () => {
   const [showFirstPassword, setShowFirstPassword] = useState(false);
   const [showSecondPassword, setShowSecondPassword] = useState(false);
   const [errors, setErrors] = useState({});
+  const [tokenValid, setTokenValid] = useState(null); // Cambiar el valor inicial a null
+  const [isLoading, setIsLoading] = useState(true); // Estado de carga
+
+  useEffect(() => {
+    const verifyToken = async () => {
+      try {
+        const response = await fetch(`http://localhost:8080/api/v1/auth/reset-password/${token}`);
+
+        if (!response.ok) {
+          setTokenValid(false);
+          return;
+        }
+
+        setTokenValid(true);
+      } catch (error) {
+        setTokenValid(false);
+      } finally {
+        // Añadir un retraso de 2 segundos
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 2000);
+      }
+    };
+
+    verifyToken();
+  }, [token]);
 
   const toggleShowFirstPassword = () => {
     setShowFirstPassword((prevShowPassword) => !prevShowPassword);
@@ -85,78 +112,87 @@ const ResetPasswordForm = () => {
 
   return (
     <div>
-      {!errors.general ? (
-        <Card className="card-register">
-          <Form onSubmit={handleSubmit}>
-            <h1>Reset password</h1>
-            <Form.Group className="mb-3" controlId="formBasicResetPassword">
-              <Form.Label>New password</Form.Label>
-              <div className="password-wrapper">
-                <Form.Control
-                  className={`password ${errors.password ? "error-border" : ""}`}
-                  type={showFirstPassword ? "text" : "password"}
-                  placeholder="*********"
-                  autoComplete="password"
-                  name="password"
-                  value={password}
-                  onChange={handlePasswordChange(setPassword)}
-                  required
-                />
-                <span className="show-password-icon" onClick={toggleShowFirstPassword}>
-                  <FontAwesomeIcon icon={showFirstPassword ? faEye : faEyeSlash} />
-                </span>
-                {errors.password && (
-                  <span className="error-icon">
-                    <FontAwesomeIcon icon={faXmark} />
-                  </span>
-                )}
-              </div>
-              {errors.password && <p className="error-message">{errors.password}</p>}
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
-              <Form.Label>Confirm new password</Form.Label>
-              <div className="password-wrapper">
-                <Form.Control
-                  className={`password ${errors.confirmPassword ? "error-border" : ""}`}
-                  type={showSecondPassword ? "text" : "password"}
-                  placeholder="*********"
-                  autoComplete="password"
-                  name="confirmPassword"
-                  value={confirmPassword}
-                  minLength={8}
-                  onChange={handlePasswordChange(setConfirmPassword)}
-                  required
-                />
-                <span className="show-password-icon" onClick={toggleShowSecondPassword}>
-                  <FontAwesomeIcon icon={showSecondPassword ? faEye : faEyeSlash} />
-                </span>
-                {errors.confirmPassword && (
-                  <span className="error-icon">
-                    <FontAwesomeIcon icon={faXmark} />
-                  </span>
-                )}
-              </div>
-              {errors.confirmPassword && <p className="error-message">{errors.confirmPassword}</p>}
-            </Form.Group>
-            <Button variant="warning" type="submit">
-              Save
-            </Button>
-            <ToastContainer
-              position="top-center"
-              closeOnClick={true}
-              pauseOnHover={true}
-            />
-          </Form>
-        </Card>
-      ) : (<div className="error-fullscreen">
-          <h1 className="error-text" >Password reset token is invalid or has expired.</h1>
+      {isLoading ? (
+        <div className="main-container-reset-password-form">
+          <Loader />
         </div>
+      ) : (
+        tokenValid ? (
+          <Card className="card-register">
+            <Form onSubmit={handleSubmit}>
+              <h1>Reset password</h1>
+              <Form.Group className="mb-3" controlId="formBasicResetPassword">
+                <Form.Label>New password</Form.Label>
+                <div className="password-wrapper">
+                  <Form.Control
+                    className={`password ${errors.password ? "error-border" : ""}`}
+                    type={showFirstPassword ? "text" : "password"}
+                    placeholder="*********"
+                    autoComplete="password"
+                    name="password"
+                    value={password}
+                    onChange={handlePasswordChange(setPassword)}
+                    required
+                  />
+                  <span className="show-password-icon" onClick={toggleShowFirstPassword}>
+                    <FontAwesomeIcon icon={showFirstPassword ? faEye : faEyeSlash} />
+                  </span>
+                  {errors.password && (
+                    <span className="error-icon">
+                      <FontAwesomeIcon icon={faXmark} />
+                    </span>
+                  )}
+                </div>
+                {errors.password && <p className="error-message">{errors.password}</p>}
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
+                <Form.Label>Confirm new password</Form.Label>
+                <div className="password-wrapper">
+                  <Form.Control
+                    className={`password ${errors.confirmPassword ? "error-border" : ""}`}
+                    type={showSecondPassword ? "text" : "password"}
+                    placeholder="*********"
+                    autoComplete="password"
+                    name="confirmPassword"
+                    value={confirmPassword}
+                    minLength={8}
+                    onChange={handlePasswordChange(setConfirmPassword)}
+                    required
+                  />
+                  <span className="show-password-icon" onClick={toggleShowSecondPassword}>
+                    <FontAwesomeIcon icon={showSecondPassword ? faEye : faEyeSlash} />
+                  </span>
+                  {errors.confirmPassword && (
+                    <span className="error-icon">
+                      <FontAwesomeIcon icon={faXmark} />
+                    </span>
+                  )}
+                </div>
+                {errors.confirmPassword && <p className="error-message">{errors.confirmPassword}</p>}
+              </Form.Group>
+              <Button variant="warning" type="submit">
+                Save
+              </Button>
+              <ToastContainer
+                position="top-center"
+                closeOnClick={true}
+                pauseOnHover={true}
+              />
+            </Form>
+          </Card>
+        ) : (
+          <div className="error-fullscreen">
+            <h1 className="error-text">Password reset token is invalid or has expired.</h1>
+          </div>
+        )
       )}
     </div>
   );
 };
 
 export default ResetPasswordForm;
+
+
 
 
 
